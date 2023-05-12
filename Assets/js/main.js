40ef9d055d008520278efa0eaabf51f61b2dbdcf
Hxzard <94038102+Hxzardd@users.noreply.github.com>
2023-05-12 13:29:32 +0530

    Update main.js

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,79 +1,79 @@
-const sleep = e => new Promise(a => setTimeout(a, e));
-const alerts = document.querySelector(".alerts");
-
-// Function to create an alert element
-function makeAlert(e, a, t) {
-  let s = Object.assign(document.createElement("div"), {
-    className: "alert"
-  });
-  s.appendChild(Object.assign(document.createElement("i"), {
-    className: e,
-    style: a
-  }));
-  s.appendChild(Object.assign(document.createElement("p"), {
-    innerHTML: t
-  }));
-  return s;
-}
-
-// Function to copy text to clipboard and display a success message
-async function copy(e) {
-  navigator.clipboard.writeText(e);
-  // Create and add an alert element to the DOM
-  let a = makeAlert("fa-regular fa-circle-check", "color: #9bfa9b", "Copied to clipboard.");
-  alerts.appendChild(a);
-  // Get the index of the newly added alert element and animate it
-  Array.from(alerts.childNodes).indexOf(a);
-  a.animate([{ opacity: 0, offset: 0 }], { duration: 200 });
-  await sleep(2000);
-  a.animate([{ opacity: 0, offset: 1 }], { duration: 200 });
-  await sleep(200);
-  a.style.height = 0;
-  a.style.opacity = 0;
-  await sleep(200);
-  // Remove the alert element from the DOM
-  alerts.removeChild(a);
-}
-
-const ageDisplay = document.querySelector(".age");
-const birth = new Date(atob("MjIgQXVnIDIwMDQgMDA6MDA6MDAgR01UKzA1OjMw"));
-
-// Function to calculate the age
-function det() {
-  let e = new Date();
-  return ((e - birth) / 1e3 / 60 / 60 / 24 / 365).toFixed(9);
-}
-
-// Function to update the age display continuously
-async function updateAge() {
-  for (;;) {
-    ageDisplay.innerHTML = det();
-    await sleep(20);
-  }
-}
-
-updateAge();
-
-const r = new XMLHttpRequest();
-r.open("GET", "https://api.lanyard.rest/v1/users/930055165480951809");
-r.send();
-r.addEventListener("load", () => {
-  let e = JSON.parse(r.responseText);
-  if (e.success && e.data.listening_to_spotify) {
-    console.log(e);
-    // Create a div element to display the currently playing Spotify song
-    let a = Object.assign(document.createElement("div"), {
-      className: "listening",
-      // Add the song details to the innerHTML of the div element
-      innerHTML: `<div class="listening-container"><i class="fa-brands fa-spotify"></i>
-        <p>Listening to <span class="title">${e.data.spotify.song}</span> by
-        <span class="artist">${e.data.spotify.artist.replaceAll(";", ",")}</span></p></div>`
+const sleep = e=>new Promise((t=>setTimeout(t, e)))
+  , alerts = document.querySelector(".alerts");
+function makeAlert(e, t, a) {
+    const n = Object.assign(document.createElement("div"), {
+        className: "alert"
     });
-    document.body.appendChild(a);
-  }
-});
+    return n.appendChild(Object.assign(document.createElement("i"), {
+        className: e,
+        style: t
+    })),
+    n.appendChild(Object.assign(document.createElement("p"), {
+        innerHTML: a
+    })),
+    n
+}
+async function copy(e, t) {
+    navigator.clipboard.writeText(t);
+    const a = makeAlert("fa-regular fa-circle-check", "color: #9bfa9b", "Copied to clipboard.");
+    alerts.appendChild(a);
+    Array.from(alerts.childNodes).indexOf(a);
+    a.animate([{
+        opacity: 0,
+        offset: 0
+    }], {
+        duration: 200
+    }),
+    await sleep(2e3),
+    a.animate([{
+        opacity: 0,
+        offset: 1
+    }], {
+        duration: 200
+    }),
+    await sleep(200),
+    a.style.height = 0,
+    a.style.opacity = 0,
+    await sleep(200),
+    alerts.removeChild(a)
+}
+function fx(e) {
+    e.animate([{
+        background: "rgba(0, 0, 0, 0.3)",
+        offset: 0
+    }], {
+        duration: 500
+    })
+}
+const ageDisplay = document.querySelector(".age")
+  , birth = new Date(atob("MjIgQXVnIDIwMDQgMDA6MDA6MDAgR01UKzA1OjMw"));
+function det() {
+    return ((new Date - birth) / 1e3 / 60 / 60 / 24 / 365).toFixed(9)
+}
+async function updateAge() {
+    for (; ; )
+        ageDisplay.innerHTML = det(),
+        await sleep(20)
+}
+updateAge();
+const wrapper = document.querySelector(".wrapper")
+  , r = new XMLHttpRequest;
+r.open("GET", "https://api.lanyard.rest/v1/users/930055165480951809"),
+r.send(),
+r.addEventListener("load", (()=>{
+    const e = JSON.parse(r.responseText);
+    if (e.success && e.data.listening_to_spotify) {
+        console.log(e);
+        const t = Object.assign(document.createElement("div"), {
+            className: "listening",
+            innerHTML: `<div class="listening-container"><i class="fa-brands fa-spotify"></i><p>Listening to <span class="title">${e.data.spotify.song}</span> by <span class="artist">${e.data.spotify.artist.replace(";", ",")}</span></p></div>`
+        });
+        document.body.appendChild(t)
+    }
+}
+));
 const ree = document.querySelector(".ree");
-for (char of "Donate below or 🔪")
+for (char of "ナイフパーティー♡")
     ree.appendChild(Object.assign(document.createElement("span"), {
         innerHTML: char
     }));
